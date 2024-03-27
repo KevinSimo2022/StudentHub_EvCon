35ed38803d393c51b4222606a37c14a5bb967a9e
KevinSimo2022 <k.takoulo@alustudent.com>
2024-03-27 16:30:52 +0200

    corrections

--- src/Components/Navbar/fetchUserData.js
+++ src/Components/Navbar/fetchUserData.js
@@ -4,8 +4,3 @@ export function fetchUserData() {
   return axios.get("https://json-server-deploy-mock.herokuapp.com/user");
 }
 
-import axios from "axios";
-
-export function fetchUserData() {
-  return axios.get("https://json-server-deploy-mock.herokuapp.com/user");
-}
